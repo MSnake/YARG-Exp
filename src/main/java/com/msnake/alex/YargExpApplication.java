9a4ed0cd682e1ccff5912f559e755da6c5c3e2cd
shigaev_ak <shigaev_ak@ursip.ru>
2017-04-11 14:46:36 +0300

    - Первоначальная настройка spring boot проекта

---
+++ src/main/java/com/msnake/alex/YargExpApplication.java
@@ -0,0 +1,12 @@
+package com.msnake.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YargExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YargExpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/msnake/alex/YargExpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msnake.alex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YargExpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
